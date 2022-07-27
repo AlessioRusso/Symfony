f746db0cb877b0f195a1f1dc277da4cc16bd7636
AlessioRusso <alessio.russo@geckosoft.it>
2022-07-27 18:02:16 +0200

    cache tag aware

--- project/src/Controller/DefaultController.php
+++ project/src/Controller/DefaultController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\Adapter\TagAwareAdapter;
+
 
 
 use App\Entity\User;
@@ -20,6 +22,52 @@ class DefaultController extends Controller
 
     public function __construct($logger){
 
+    }
+
+
+    /**
+     * @Route("/Tag", name="tag")
+     */
+    public function tag()
+    {
+
+        $cache = new TagAwareAdapter(
+            new FilesystemAdapter()
+        );
+
+        $acer = $cache->getItem('acer');
+        $dell = $cache->getItem('dell');
+    
+
+        if (!$acer->isHit())
+        {
+            $acer_from_db = 'acer';
+            $acer->set($acer_from_db);
+            $acer->tag(['computers', 'laptops', 'acer']);
+            $cache->save($acer);
+            dump('acer laptop from database ... ');
+        }
+
+        if (!$dell->isHit())
+        {
+            $dell_from_db = 'dell';
+            $dell->set($dell_from_db);
+            $dell->tag(['computers', 'notebook', 'dell']);
+            $cache->save($dell);
+            dump('dell laptop from database');
+        }
+
+
+        $cache->invalidateTags(['computers']);
+
+        dump($acer->get());
+        dump($dell->get());
+      
+
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
+            'users' => [],
+        ]);
     }
 
 
