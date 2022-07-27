55bd00406d9ad16cb597fbaaf6cc9c2213446a2a
AlessioRusso <alessio.russo@geckosoft.it>
2022-07-27 16:51:25 +0200

    add cache

--- project/src/Controller/DefaultController.php
+++ project/src/Controller/DefaultController.php
@@ -7,12 +7,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 
 use App\Entity\User;
 
 use App\Services\GiftsService;
-
+use Doctrine\ORM\Mapping\Cache;
 
 class DefaultController extends Controller
 {
@@ -27,6 +28,25 @@ class DefaultController extends Controller
      */
     public function index()
     {
+
+        $cache = new FilesystemAdapter();
+        $posts = $cache->getItem('database.get_posts');
+
+        if (!$posts->isHit())
+        {
+            $posts_from_db = ['post 1', 'post 2', 'post 3'];
+            dump('Connected with database...');
+
+            $posts->set(serialize($posts_from_db));
+            $posts->expiresAfter(5);
+            $cache->save($posts);
+        }
+
+        // $cache->deleteItem('database.get_posts');
+        // $cache->clear();
+
+        dump(unserialize($posts->get()));
+
         
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
